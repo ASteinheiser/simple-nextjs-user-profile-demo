309ac15b43bd3b6aca4ed5bb93b9db20e0159da6
ASteinheiser <me@iamandrew.io>
2025-03-28 13:15:52 -0700

    style the UserRow component

--- src/components/UserRow.tsx
+++ src/components/UserRow.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import type { User } from "@/app/api/graphql/user-data";
 
@@ -8,8 +9,21 @@ interface UserRowProps {
 export default function UserRow({ user }: UserRowProps) {
   return (
     <Link href={`/user/${user.id}`} key={user.id}>
-      <div>
-        <h2>{user.name}</h2>
+      <div className="flex flex-row items-center justify-between shadow-md rounded-lg p-4">
+        <div className="flex flex-row items-center gap-6">
+          <Image
+            src={`/profile-${user.profilePictureVariant}.svg`}
+            alt={user.name}
+            width={120}
+            height={120}
+          />
+
+          <h2 className="text-lg">{user.name}</h2>
+        </div>
+
+        <span className="text-md">
+          {user.friends.length} friend{user.friends.length === 1 ? '' : 's'}
+        </span>
       </div>
     </Link>
   );
