9a891061d91796c69ac76d6d058a5bf9dd45cdc1
ASteinheiser <me@iamandrew.io>
2025-03-28 13:32:45 -0700

    finish styling profile page

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,5 +1,9 @@
+import Image from "next/image";
+import Link from "next/link";
 import { gql } from "@apollo/client";
 import { createGraphClient } from "@/client-api/graph-client";
+import type { User } from "@/app/api/graphql/user-data";
+import UserRow from "@/components/UserRow";
 
 interface UserPageProps {
   params: Promise<{ id: string }>;
@@ -31,5 +35,37 @@ export default async function UserPage({ params }: UserPageProps) {
     return <div>Error: {error.message}</div>;
   }
 
-  return <div>User Page: {data?.user?.name ?? ''}</div>;
+  return (
+    <div className="flex flex-col gap-8 max-w-lg m-auto p-6">
+      <div className="flex flex-row items-center gap-6">
+        <Link href="/">
+          <Image
+            src={`/arrow-back.svg`}
+            alt={'arrow back'}
+            width={48}
+            height={48}
+          />
+        </Link>
+
+        <h1 className="text-xl font-title">{data?.user?.name}</h1>
+      </div>
+
+      <Image
+        src={`/profile-${data?.user?.profilePictureVariant}.svg`}
+        alt={data?.user?.name ?? ''}
+        width={200}
+        height={200}
+      />
+
+      <div className="flex flex-col gap-6">
+        <h2 className="text-xl font-title">Friends</h2>
+
+        <div className="w-full h-[2px] bg-secondary" />
+
+        {(data?.user?.friends ?? []).map((friend: User) => (
+          <UserRow key={friend.id} user={friend} />
+        ))}
+      </div>
+    </div>
+  );
 }
